ae4630ab0e0f23f338f29712927157272bcb8e72
mappia <gerald.arocena@gmail.com>
2020-09-10 22:15:37 -0400

    Did problem 3f.

--- homework/HW1/P3.sh
+++ homework/HW1/P3.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 grep [0-9] apollo13.txt | wc -l
 grep --help | grep "\-\-\c\o\u\n\t"
-find . -maxdepth 1 -type f -name "*.py" | wc -l     # referenced https://stackoverflow.com/questions/7715485/how-to-only-find-files-in-a-given-directory-and-ignore-subdirectories-using-bas
-find . -type f ! -perm -o=r,o=w | wc -l             # referenced https://ostechnix.com/find-files-based-permissions/ && https://unix.stackexchange.com/questions/425205/how-can-i-find-all-files-i-do-not-have-write-access-to-in-specific-folder
+find . -maxdepth 1 -type f -name "*.py" | wc -l
+find . -type f ! -perm -o=r,o=w | wc -l
 find . -maxdepth 1 ! -perm -o=r,o=w | wc -l
+
+# References:
+# 1. https://stackoverflow.com/questions/7715485/how-to-only-find-files-in-a-given-directory-and-ignore-subdirectories-using-bas
+# 2. https://ostechnix.com/find-files-based-permissions/ && https://unix.stackexchange.com/questions/425205/how-can-i-find-all-files-i-do-not-have-write-access-to-in-specific-folder
---
+++ homework/HW1/P3F.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for file in *
+do
+    if [ -f $file ]; then
+        echo "$file $(< $file wc -l)"
+    fi
+done
+
+# References:
+# 1. https://unix.stackexchange.com/questions/351210/loop-through-files-excluding-directories
+# 2. https://unix.stackexchange.com/questions/126998/getting-the-line-count-and-storing-in-a-result-set
