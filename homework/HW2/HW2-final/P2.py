9f22c982c68903c0168d8cba396d7575c16b996c
mappia <gerald.arocena@gmail.com>
2020-09-21 08:05:19 -0400

    Did problem 2.

---
+++ homework/HW2/HW2-final/P2.py
@@ -0,0 +1,42 @@
+# dna_complement(sequence)
+# Takes a DNA sequence and returns its complement
+def dna_complement(sequence):
+    Bases = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    complement = ""
+    valid_seq = True
+    if sequence:
+        for base in sequence:
+            # TODO: Can sequences have spaces?
+            # TODO: Truncate leading/trailing space?
+            if base.upper() in Bases:
+                complement = complement + Bases.get(base.upper())
+            else:
+                valid_seq = False
+                break
+    else:
+        valid_seq = False
+    # TODO: The function needs to return None (the object)
+    if valid_seq:
+        return complement
+    else:
+        return None
+
+# Demo of dna_complement(sequence)
+valid_input = "aattggcc"
+invalid_input = "aattggzz"
+print("Example input string: \"", valid_input, "\"", sep="")
+print("Complement is: \"", dna_complement(valid_input), "\"", sep="")
+print("Example input string: \"", invalid_input, "\"", sep="")
+print("Complement is:", dna_complement(invalid_input))
+
+# References
+# https://www.w3schools.com/python/python_functions.asp
+# https://stackoverflow.com/questions/20875150/how-to-fill-an-empty-string-which-has-already-been-created-in-python
+# https://www.w3schools.com/python/python_booleans.asp
+# https://www.digitalocean.com/community/tutorials/how-to-use-break-continue-and-pass-statements-when-working-with-loops-in-python-3
+# https://www.geeksforgeeks.org/python-string-upper/
+# https://www.geeksforgeeks.org/get-method-dictionaries-python/#:~:text=In%20python%20dictionaries%2C%20following%20is,a%20value%20for%20a%20key.&text=The%20get()%20method%20is,used%20with%20only%20one%20argument).
+# https://www.educative.io/edpresso/how-to-check-if-a-key-exists-in-a-python-dictionary
+# https://www.tutorialspoint.com/python/python_functions.htm
+# https://stackoverflow.com/questions/9050355/using-quotation-marks-inside-quotation-marks
+# https://www.poftut.com/python-how-to-print-without-newline-or-space/#:~:text=Change%20separator,will%20join%20two%20string%20together.
