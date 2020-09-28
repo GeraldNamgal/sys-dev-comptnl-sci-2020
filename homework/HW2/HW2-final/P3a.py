edb750a42bcfb11cdcdd7c67eabc3eff0f2bde50
mappia <gerald.arocena@gmail.com>
2020-09-28 00:31:45 -0400

    Made changes to P3 and P4 files.

--- homework/HW2/HW2-final/P3a.py
+++ homework/HW2/HW2-final/P3a.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+print("This program does not behave correctly (under the hood) because "
+      + "it's not updating the value of the 'balance' object stored in memory "
+      + "that was passed in to the outer function 'make_withdrawal()'. The "
+      + "value of the object 'balance' refers to in this case is set on the "
+      + "call to 'make_withdrawal()' when it is captured by the inner function "
+      + "after which it can not be changed via calls to the returned inner "
+      + "'closure' function. An error would occur if 'balance' was reassigned "
+      + "in the inner function. This process is called encapsulation which is "
+      + "useful for protecting against modifying state incorrectly.\n")
+
+
 def make_withdrawal(balance):
     def closure(withdraw_amt):
         print("Initial balance is", balance)
@@ -21,14 +32,3 @@ new_withdrawal_amount = 10
 wd = make_withdrawal(init_balance)
 wd(withdrawal_amount)
 wd(new_withdrawal_amount)
-
-
-print("\nThe above program does not behave correctly (under the hood) because "
-      + "it's not updating the value of the 'balance' object stored in memory "
-      + "that was passed in to the outer function 'make_withdrawal()'. The "
-      + "value of the object 'balance' refers to in this case is set on the "
-      + "call to 'make_withdrawal()' when it is captured by the inner function "
-      + "after which it can not be changed via calls to the returned inner "
-      + "'closure' function. An error would occur if 'balance' was reassigned "
-      + "in the inner function. This process is called encapsulation which is "
-      + "useful for protecting against modifying state incorrectly.")
--- homework/HW2/HW2-final/P4.py
+++ homework/HW2/HW2-final/P4.py
@@ -2,3 +2,10 @@
 import numpy
 import matplotlib.pyplot
 import datetime
+import math
+
+def clock_hand(hand_length):
+    def closure(theta):
+        theta_radians = theta * (numpy.pi/180)
+        x_coord = hand_length * numpy.cos(theta_radians)
+    return closure
