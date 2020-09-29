05a83aa614fe8886508d31ce63bfbe8f4f2e405f
mappia <gerald.arocena@gmail.com>
2020-09-29 17:25:24 -0400

    Updated comment in P4 animation.

--- homework/HW2/HW2-final/P4_animation.py
+++ homework/HW2/HW2-final/P4_animation.py
@@ -15,7 +15,7 @@ def clock_hand(hand_length):
 
 fig = plt.figure(figsize=(6, 6))           # Defining figure window to use/reuse
 start_time = None                          # Initialize start time of animation
-run_duration = 64                          # To be sure to go for at least 1 min
+run_duration = 64                          # Run for at least 1 min (60+ secs)
 end_time = None                            # Initialize end time of animation
 
 # Running animation...
