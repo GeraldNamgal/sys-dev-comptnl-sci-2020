0323eb331289781865427124df34c519d12bba7c
mappia <gerald.arocena@gmail.com>
2020-10-06 22:18:24 -0400

    Saving progress.

---
+++ homework/HW3/HW3-final/Regression.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class Regression:
+    def __init__(self):
+        self.params = {}
+
+    def get_params(self):
+        return self.params
+
+    def set_params(self, **kwargs):
+        # TODO: Manually set parameters of the linear model
+
+    def fit(self, X, y):
+
+
+    def predict(self, X):
+
+
+    def score(self, X, y):
