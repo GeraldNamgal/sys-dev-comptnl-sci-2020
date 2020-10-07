453d9565cafa65926d002e3385cf0f04b8ed199e
mappia <gerald.arocena@gmail.com>
2020-10-07 17:33:03 -0400

    Changed and moved Regression.py and also added pycache/ to .gitignore file.

---
+++ homework/HW3/HW3-final/regression/Regression.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class Regression:
+    def __init__(self):
+        self.params = {}
+
+    def get_params(self):
+        return self.params
+
+    def set_params(self, **kwargs):
+        raise NotImplementedError("Method not implemented yet")
+
+    def fit(self, X, y):
+        raise NotImplementedError("Method not implemented yet")
+
+    def predict(self, X):
+        raise NotImplementedError("Method not implemented yet")
+
+    def score(self, X, y):
+        raise NotImplementedError("Method not implemented yet")
+
+# TODO: Factor out common code back into base class when finished derived classes
