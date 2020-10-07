08d8c978c14c521e7b31c6a2f098ede09c31e16b
mappia <gerald.arocena@gmail.com>
2020-10-07 15:16:23 -0400

    Pushing...

--- homework/HW3/HW3-final/Regression.py
+++ homework/HW3/HW3-final/Regression.py
@@ -9,11 +9,18 @@ class Regression:
 
     def set_params(self, **kwargs):
         # TODO: Manually set parameters of the linear model
+        raise NotImplementedError
 
     def fit(self, X, y):
-
+        raise NotImplementedError
 
     def predict(self, X):
-
+        raise NotImplementedError
 
     def score(self, X, y):
+        raise NotImplementedError
+
+
+a_test = Regression()
+try:
+    a_test.fit(5, 3)
