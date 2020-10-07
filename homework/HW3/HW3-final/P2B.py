03680270c1f95fadd881c77b50fecc3458711a11
mappia <gerald.arocena@gmail.com>
2020-10-07 19:19:22 -0400

    Done P2B.

--- homework/HW3/HW3-final/P2B.py
+++ homework/HW3/HW3-final/P2B.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-import regression.Regression as regr
+from regression.Regression import Regression as RegressionAlias
+import types
 
-a = regr.Regression()
-a.hello_world()
+# Get and print all the functions of the Regression class
+attributes = dir(RegressionAlias)
+print("Functions in Regression class:")
+for attribute in attributes:
+    if isinstance(getattr(RegressionAlias, attribute), types.FunctionType):
+        print(attribute)
+
+
+# References:
+# - https://stackoverflow.com/questions/1091259/how-to-test-if-a-class-attribute-is-an-instance-method
