66be4ff03f62a967f06ceef545cec259a330221c
mappia <gerald.arocena@gmail.com>
2020-10-08 18:53:40 -0400

    Did debugging output to compare pandas with sklearn.

--- homework/HW3/HW3-final/regression/Regression.py
+++ homework/HW3/HW3-final/regression/Regression.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import pandas as pd  # TODO: Delete later
+import warnings  # TODO: Delete later
+warnings.filterwarnings('ignore')  # TODO: Delete later
+from sklearn import datasets  # TODO: Goes in other file
+from sklearn.model_selection import train_test_split  # TODO: Goes in other file
 
 
 class Regression:
@@ -26,28 +31,25 @@ class Regression:
 
 
 class LinearRegression(Regression):
-    # TODO
+    # TODO: Belongs in OLS or just Ridge?
     def set_params(self, **kwargs):
         raise NotImplementedError('Method not implemented yet')
 
     # TODO
     def fit(self, X, y):
         ones_col = np.ones(shape=X.shape[0]).reshape(-1, 1)
-        X = np.append(ones_col, X, axis=1)
+        X = np.append(ones_col, X, axis=1)       # Append column of ones to X
+        # Ordinary Least Squares best fit coefficients equation aka 'beta_hat':
         beta_hat = np.linalg.pinv(X.transpose().dot(X)).dot(X.transpose()).dot(y)
-        # TODO: Debugging
-        print(beta_hat, '\n')
-        # Intercept can be thought of as beta_0
-        self.params['intercept'] = beta_hat[0]
-        # TODO: Debugging
-        print(self.params['intercept'], '\n')
-        self.params['coeffs'] = beta_hat[1:]
-        # TODO: Debugging
-        print(self.params['coeffs'])
+        self.params['coeffs'] = beta_hat[1:]     # Non-first rows of matrix
+        self.params['intercept'] = beta_hat[0]   # Intercept is also 'beta_0'
 
     # TODO
     def predict(self, X):
-        raise NotImplementedError('Method not implemented yet')
+        prediction = self.params['intercept']
+        for xi, bi in zip(X, self.params['coeffs']):
+            prediction += (bi * xi)
+        return prediction
 
     # TODO
     def score(self, X, y):
@@ -55,10 +57,24 @@ class LinearRegression(Regression):
 
 
 # TODO: Debugging
-X = np.array([[0, 1, 2, 3], [1, 5, 3, 7], [1, 7, 3, 8], [9, 2, 4, 8], [5, 9, 1, 3]])
-y = np.array([[3],[7],[1],[6],[9]])
-regr = LinearRegression()
-regr.fit(X, y)
+# Get Boston data using pandas
+boston = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv')
+print(boston.head(), '\n')
+X = boston.drop('medv', axis=1).values
+y = boston['medv'].values
+print(X, '\n')
+print(y, '\n')
+
+# Get Boston data using hw instructions
+dataset = datasets.load_boston()
+print(dataset['data'], '\n')
+print(dataset['target'], '\n')
+
+X_train, X_test, y_train, y_test = train_test_split(dataset['data'],
+                                                    dataset['target'],
+                                                    test_size=0.2,
+                                                    random_state=42)
+
 
 # References:
 # - https://towardsdatascience.com/multiple-linear-regression-from-scratch-in-numpy-36a3e8ac8014
