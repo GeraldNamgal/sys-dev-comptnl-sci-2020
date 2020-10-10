ee83cff878fba9dfadbafcbcf080d3b66aea2cd2
mappia <gerald.arocena@gmail.com>
2020-10-10 12:47:02 -0400

    Checking what changed.

--- homework/HW3/HW3-final/model_scoring.py
+++ homework/HW3/HW3-final/model_scoring.py
@@ -5,16 +5,15 @@ from sklearn.model_selection import train_test_split
 import regression.Regression as RegrModuleAlias
 
 
-# Get Boston data
-dataset = datasets.load_boston()
+dataset = datasets.load_boston()           # Get Boston data
 
-# Do train-test split on the data
+# Do train-test split on the data...
 X_train, X_test, y_train, y_test = train_test_split(dataset['data'],
                                                     dataset['target'],
                                                     test_size=0.2,
                                                     random_state=42)
 
-# Score the models
+# Score the models...
 alpha = 0.1
 models = [RegrModuleAlias.LinearRegression(alpha),
           RegrModuleAlias.RidgeRegression(alpha)]
@@ -24,7 +23,7 @@ for idx, model in enumerate(models):
     model.fit(X_train, y_train)
     score = model.score(X_test, y_test)
     print(type(model).__name__, "model's R-squared score is:", score)
-    if score > high_score:                 # Is current highest score?
+    if score > high_score:                 # Is the current highest score?
         high_score = score
         model_idx = idx
 if high_score > 0:                         # If highest score is > 0
