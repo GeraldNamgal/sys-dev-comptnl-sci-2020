f17eaa4b4f344375e29308d52b1cb61ff014f1b9
mappia <gerald.arocena@gmail.com>
2020-10-27 21:04:30 -0400

    Fixed exercise 2.

--- pair_programming/PP7/exercise_2.py
+++ pair_programming/PP7/exercise_2.py
@@ -1,20 +1,17 @@
+#!/usr/bin/env python3
+
 # Sharer, Listener, Coder: Nishu Lahoti, Bianca Cordazzo, Gerald Arocena
 
 # Exercise 2
 
-import numpy as np
-
-x = 3
-r = 4
-
-
 def my_pow(r):
-    def inner(f, seed_value=1): 
-        return r*(x**(r-1))*seed_value
+    def inner(eval_point, seed_value=1):
+        return r * (eval_point ** (r-1)) * seed_value
     return inner
 
 
+# Demoing my_pow --
 if __name__ == "__main__":
+    x, r = 3, 4
     example = my_pow(r)
-    example(x**r)
-    print(example(x**r))
+    print(example(x))                      # Should be 108
