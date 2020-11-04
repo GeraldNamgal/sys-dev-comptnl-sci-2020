2873d6078afd93acb9793047b6ec8409b3a5b88f
mappia <gerald.arocena@gmail.com>
2020-11-04 08:33:34 -0500

    Added comments to Animal.py.

--- homework/HW5/HW5-final/animal.py
+++ homework/HW5/HW5-final/animal.py
@@ -20,16 +20,16 @@ class Animal:
 
     def __init__(self, name, species):
         self.name = name
-        self._species = species
+        self._species = species     # Non-user-facing attribute (has underscore)
 
     def __repr__(self):
         return f'{self.name} ({self._species})'
 
-    @property
+    @property                       # Allows user to directly 'get' species
     def species(self):
         return self._species
 
-    @species.setter
+    @species.setter                 # Allows user to directly 'set' species
     def species(self, into):
         assert into in Animal.valid_species,\
             Exception(f'invalid species: {into}')
