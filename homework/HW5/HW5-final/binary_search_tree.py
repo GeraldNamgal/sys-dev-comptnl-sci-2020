7c6f69a4fb82153c1c9ecaa2eec4ac265d101868
mappia <gerald.arocena@gmail.com>
2020-11-09 17:45:45 -0500

    Changed BST to a boolean way of working.

--- homework/HW5/HW5-final/binary_search_tree.py
+++ homework/HW5/HW5-final/binary_search_tree.py
@@ -16,6 +16,7 @@ class BSTNode:
 class BSTTable:
     def __init__(self):
         self._root = None
+        self.added_node = False          # TODO: Test the new boolean way
 
     def __str__(self):
         return str(self._root)
@@ -25,15 +26,25 @@ class BSTTable:
 
     def put(self, key, val):
         self._root = self._put(self._root, key, val)
+        if self.added_node:              # Reset added_node?
+            self.added_node = False
 
     def get(self, key):
         return self._get(self._root, key)
 
     def _put(self, node, key, val):
-        try:
-            node = update_node(node, key, val)     # Update node if key exists
-        except KeyError:
-            node = add_node(node, key, val)        # Else add new node to tree
+        if node is None:                 # Add new node?
+            self.added_node = True
+            return BSTNode(key, val)
+        if node.key == key:              # Update existing node?
+            node.val = val
+            return node
+        if node.key > key:
+            node.left = self._put(node.left, key, val)
+        if node.key < key:
+            node.right = self._put(node.right, key, val)
+        if self.added_node:              # Update node size?
+            node.size += 1
         return node
 
     # TODO: Check that updating size of nodes is working right
@@ -53,32 +64,6 @@ class BSTTable:
         return node.size if node else 0
 
 
-# Utility function
-def update_node(node, key, new_val):
-    if node is None:
-        raise KeyError
-    if node.key == key:
-        node.val = new_val                         # Change node's value
-        return node
-    if node.key > key:
-        node.left = update_node(node.left, key, new_val)
-    else:
-        node.right = update_node(node.right, key, new_val)
-    return node
-
-
-# Utility function
-def add_node(node, key, val):
-    if node is None:
-        return BSTNode(key, val)                   # Add new node
-    if node.key > key:
-        node.left = add_node(node.left, key, val)
-    if node.key < key:
-        node.right = add_node(node.right, key, val)
-    node.size += 1                                 # Update node size
-    return node
-
-
 # TODO: Debugging...
 # greektoroman = BSTTable()
 # greektoroman.put('Athena',    'Minerva')
@@ -94,19 +79,19 @@ def add_node(node, key, val):
 # print()
 # print(greektoroman)
 
-tree = BSTTable()
-list = [13,7,19,17,3,29,5,31,2,11]
-# list = [7,19,17,3,29,5,31,2,11]
-# list = [13,7,19,17,3,5,31,2,11]
-# list = [13,7,19,17,3,29,5,2,11]
-for num in list:
-    tree.put(num, num)
-print(tree)
-tree.get(17)
-tree.put(29, 'update')
-print(tree)
-tree.get(17)                      # Size should stay the same for just updates
-tree.put(70, 70)
-print(tree)
-tree.get(70)                     # TODO: Size should change for new nodes
-tree.get(83)                     # TODO: Should throw KeyError if node isn't found
+# tree = BSTTable()
+# list = [13,7,19,17,3,29,5,31,2,11]
+# # list = [7,19,17,3,29,5,31,2,11]
+# # list = [13,7,19,17,3,5,31,2,11]
+# # list = [13,7,19,17,3,29,5,2,11]
+# for num in list:
+#     tree.put(num, num)
+# print(tree)
+# tree.get(17)
+# tree.put(29, 'update')
+# print(tree)
+# tree.get(17)                      # Size should stay the same for just updates
+# tree.put(70, 70)
+# print(tree)
+# tree.get(70)                     # TODO: Size should change for new nodes
+# # tree.get(83)                     # TODO: Should throw KeyError if node isn't found
