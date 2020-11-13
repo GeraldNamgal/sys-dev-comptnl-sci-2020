af1dd08d24194ffd4d97708e7f43318eb22199c2
mappia <gerald.arocena@gmail.com>
2020-11-13 18:02:08 -0500

    Saving progress on P1.py.

---
+++ homework/HW6/HW6-final/P1.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+class BSTNode:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.left, self.right = None, None
+        self.size = 1
+
+    def __str__(self):
+        return f'BSTNode({self.key}, {self.val})' + \
+               '\n|\n|-(L)->' + '\n|      '.join(str(self.left).split('\n')) + \
+               '\n|\n|-(R)->' + '\n|      '.join(str(self.right).split('\n'))
+
+
+class BSTTable:
+    def __init__(self):
+        self._root = None
+
+    def __str__(self):
+        return str(self._root)
+
+    def __len__(self):
+        return self._size(self._root)
+
+    def put(self, key, val):
+        self._root = self._put(self._root, key, val)
+
+    def get(self, key):
+        return self._get(self._root, key)
+
+    def _put(self, node, key, val):
+        if not node:
+            return BSTNode(key, val)
+        if key < node.key:
+            node.left = self._put(node.left,  key, val)
+        elif key > node.key:
+            node.right = self._put(node.right, key, val)
+        else:
+            node.val = val
+        node.size = 1 + self._size(node.left) + self._size(node.right)
+        return node
+
+    def _get(self, node, key):
+        if not node:
+            raise KeyError(f'key not found: {key}')
+        if key < node.key:
+            return self._get(node.left,  key)
+        elif key > node.key:
+            return self._get(node.right, key)
+        else:
+            return node.val
+
+    @staticmethod
+    def _size(node):
+        return node.size if node else 0
+
+    def _removemin(self, node):
+        if not node:
+            raise Exception(f'Tree is empty')
+        if not node.left:                           # Found the min?
+            # TODO: Do we need to delete (like Null out or something) the node itself?
+            if node.right:                          # Has a right child?
+                node = node.right
+            else:                                   # Else return None
+                node = None
+        else:
+            node.left = self._removemin(node.left)
+        # TODO: if removed_node, then node_size -= 1
+
+
+# TODO: Debugging...
+t = BSTTable()
+t.put(5, 'a')
+t.put(1, 'b')
+t.put(2, 'c')
+t.put(0, 'd')
+print(t._root)
+print(t._removemin(t._root))
