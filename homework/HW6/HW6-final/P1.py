08464c85dc76d9cdb714e7aa14bc21ae751b7979
mappia <gerald.arocena@gmail.com>
2020-11-14 00:34:15 -0500

    Think done up to Prob 1 Part B inclusive.

--- homework/HW6/HW6-final/P1.py
+++ homework/HW6/HW6-final/P1.py
@@ -6,8 +6,9 @@ class BSTNode:
         self.left, self.right = None, None
         self.size = 1
 
+    # TODO: Remove size printing when submit
     def __str__(self):
-        return f'BSTNode({self.key}, {self.val})' + \
+        return f'BSTNode({self.key}, {self.val}) [SIZE={self.size}]' + \
                '\n|\n|-(L)->' + '\n|      '.join(str(self.left).split('\n')) + \
                '\n|\n|-(R)->' + '\n|      '.join(str(self.right).split('\n'))
 
@@ -15,6 +16,7 @@ class BSTNode:
 class BSTTable:
     def __init__(self):
         self._root = None
+        self._removednode = False
 
     def __str__(self):
         return str(self._root)
@@ -54,25 +56,91 @@ class BSTTable:
     def _size(node):
         return node.size if node else 0
 
+    # TODO: Check that this works with other trees like one in notebook?
     def _removemin(self, node):
         if not node:
-            raise Exception(f'Tree is empty')
-        if not node.left:                           # Found the min?
-            # TODO: Do we need to delete (like Null out or something) the node itself?
-            if node.right:                          # Has a right child?
-                node = node.right
-            else:                                   # Else return None
+            return node
+        elif not node.left:             # At the min node?
+            if node.right:              # Has a right child?
+                node = node.right         # TODO: Switch this with node = None stuff?
+                # tmp_node = node.right   # TODO: This node = None stuff necessary?
+                # node = None
+                # return tmp_node
+            else:                       # No children?
                 node = None
+                # return None             # TODO: Need return (see above)?
         else:
             node.left = self._removemin(node.left)
-        # TODO: if removed_node, then node_size -= 1
+            node.size -= 1                # TODO: Double-check that size is updating correctly
+        return node
+
+    def remove(self, key):
+        self._root = self._remove(self._root, key)
+        if self._removednode:           # Reset _removednode?
+            self._removednode = False
+
+    # TODO
+    def _remove(self, node, key):
+        if not node:
+            raise KeyError(f'key not found: {key}')
+        if key < node.key:
+            node.left = self._remove(node.left, key)
+            if self._removednode:
+                node.size -= 1
+        elif key > node.key:
+            node.right = self._remove(node.right, key)
+            if self._removednode:
+                node.size -= 1
+        else:                           # Found node to remove
+            if node.left is None and node.right is None:
+                node = None
+            elif node.left is None:
+                node = node.right
+            elif node.right is None:
+                node = node.left
+            else:   # TODO: Test this out (Check that size is updating correctly, etc.)
+                min_node = self._getmin(node.right)     # Min from right child
+                node.key = min_node.key
+                node.val = min_node.val
+                node.size -= 1
+                node.right = self._removemin(node.right)
+            self._removednode = True
+        return node
+
+    def _getmin(self, node):
+        if not node:
+            return node
+        elif not node.left:             # At the min node?
+            return node
+        else:
+            return self._getmin(node.left)
 
 
 # TODO: Debugging...
+# # Part A
+# t = BSTTable()
+# t.put(5, 'a')
+# t.put(1, 'b')
+# t.put(2, 'c')
+# t.put(0, 'd')
+# print(t._root)
+# print()
+# print(t._removemin(t._root))
+# t.put(0, 'd')
+# t.put(0.5, '?')
+# print()
+# print(t._root)
+# print()
+# print(t._removemin(t._root))
+# Part B
 t = BSTTable()
 t.put(5, 'a')
 t.put(1, 'b')
 t.put(2, 'c')
 t.put(0, 'd')
 print(t._root)
-print(t._removemin(t._root))
+print()
+print(t._remove(t._root, 5))
+print()
+print(t._remove(t._remove(t._root, 5), 1))
+# print(t._remove(t._root, 10))   # Should return an error
