c61053d5990a5d35b231d876931b49ebc663ce5a
mappia <gerald.arocena@gmail.com>
2020-11-23 18:58:12 -0500

    Fixed up PP11 a bit.

--- pair_programming/PP11/PP11.py
+++ pair_programming/PP11/PP11.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
+
 # Sharer: Gerald Arocena
 # Coder: Bianca Cordazzo
 # Listener: Gerald Arocena and Bianca Cordazzo
 
 import sqlite3
 
-db = sqlite3.connect('test_db.sqlite') # Create a connection to the database
+db = sqlite3.connect('test_db.sqlite')  # Create a connection to the database
 cursor = db.cursor() 
-cursor.execute("DROP TABLE IF EXISTS candidates") 
+cursor.execute("DROP TABLE IF EXISTS candidates")  # TODO: Don't exactly understand why sometimes need this?
 
 cursor.execute('''CREATE TABLE candidates (
                id INTEGER PRIMARY KEY NOT NULL, 
@@ -17,21 +19,21 @@ cursor.execute('''CREATE TABLE candidates (
 
 db.commit()
 
-with open ("candidates.txt") as candidates:
-    next(candidates) # jump over the header
+with open("candidates.txt") as candidates:
+    next(candidates)  # jump over the header
 
     for line in candidates:
         line = line.strip('\n')
         values = line.split(sep="|")
 
         cursor.execute('''INSERT INTO candidates
-               (id, first_name, last_name, middle_init, party)
-               VALUES (?, ?, ?, ?, ?)''', 
-                (int(values[0]), values[1], values[2], values[3], values[4]))
+        (id, first_name, last_name, middle_init, party)
+        VALUES (?, ?, ?, ?, ?)''',
+                       (int(values[0]), values[1], values[2], values[3], values[4]))
 
         db.commit()
 
-# Demo
+# # Demo
 # cursor.execute("SELECT * FROM candidates")
 # all_rows = cursor.fetchall()
 # print(all_rows)
