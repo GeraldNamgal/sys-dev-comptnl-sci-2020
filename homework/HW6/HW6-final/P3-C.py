b305667607ea96421fb7e40c09ae221d58803e4f
mappia <gerald.arocena@gmail.com>
2020-11-22 23:03:13 -0500

    Pretty much got the gist of P3C done.

--- homework/HW6/HW6-final/P3-C.py
+++ homework/HW6/HW6-final/P3-C.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 from P3 import *
+from matplotlib import pyplot as plt
 
+ns = (10, 20, 50, 100, 200, 500)  # TODO: Define the lists sequence
 
-lists = timeit(pqclass=NaivePriorityQueue)
-print(lists)
-lists = timeit(pqclass=HeapPriorityQueue)
-print(lists)
-lists = timeit(pqclass=PythonHeapPriorityQueue)
-print(lists)
+npq_lists = timeit(ns, pqclass=NaivePriorityQueue)
+print(npq_lists)
+hpq_lists = timeit(ns, pqclass=HeapPriorityQueue)
+print(hpq_lists)
+ppq_lists = timeit(ns, pqclass=PythonHeapPriorityQueue)
+print(ppq_lists)
+
+# TODO: Graphs
+# Plot the results (Elapsed Time vs. ns)
+plt.plot(ns, npq_lists, label='npq')
+plt.plot(ns, hpq_lists, label='hpq')
+plt.plot(ns, ppq_lists, label='ppq')
+plt.title('Elapsed Time Vs. np for 3 Priority Queues')
+plt.xlabel('ns')
+plt.ylabel('Elapsed Time')
+plt.legend()
+plt.show()
