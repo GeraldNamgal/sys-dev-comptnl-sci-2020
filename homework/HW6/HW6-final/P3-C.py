3d650e1e571ab22e6475d93d65757c049f523d03
mappia <gerald.arocena@gmail.com>
2020-11-22 22:05:12 -0500

    Saving progress. Understand the gist of P3C.

--- homework/HW6/HW6-final/P3-C.py
+++ homework/HW6/HW6-final/P3-C.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+from P3 import *
+
+
+lists = timeit(pqclass=NaivePriorityQueue)
+print(lists)
+lists = timeit(pqclass=HeapPriorityQueue)
+print(lists)
+lists = timeit(pqclass=PythonHeapPriorityQueue)
+print(lists)
