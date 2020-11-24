0459f5ab9713960573e3cb462a5e624fa40ea291
mappia <gerald.arocena@gmail.com>
2020-11-24 01:33:42 -0500

    Pretty much done P3C.

--- homework/HW6/HW6-final/P3-C.py
+++ homework/HW6/HW6-final/P3-C.py
@@ -3,24 +3,23 @@
 from P3 import *
 from matplotlib import pyplot as plt
 
-ns = (0, 10, 20, 50, 100, 200, 500)  # TODO: Define the lists sequence -- any changes needed?
+# TODO: Define the lists sequence -- any changes needed?
+ns = (0, 10, 20, 50, 100, 200, 500)
 
 npq_lists = timeit(ns, pqclass=NaivePriorityQueue)
 print(npq_lists)
 hpq_lists = timeit(ns, pqclass=HeapPriorityQueue)
 print(hpq_lists)
-ppq_lists = timeit(ns, pqclass=PythonHeapPriorityQueue)
-print(ppq_lists)
+phpq_lists = timeit(ns, pqclass=PythonHeapPriorityQueue)
+print(phpq_lists)
 
-# TODO: What graphs should look like -- https://piazza.com/class/kc57xuuysdm64b?cid=473
-
-# TODO: Graphs -- Clean this up more and stuff
+# TODO: Graphs -- Clean this up more and stuff?
 # Plot the results (Elapsed Time vs. ns)
-plt.plot(ns, npq_lists, label='npq')
-plt.plot(ns, hpq_lists, label='hpq')
-plt.plot(ns, ppq_lists, label='ppq')
-plt.title('Elapsed Time Vs. np for 3 Priority Queues')
-plt.xlabel('ns')
-plt.ylabel('Elapsed Time')
+plt.plot(ns, npq_lists, label='Naive Priority Queue')
+plt.plot(ns, hpq_lists, label='Heap Priority Queue')
+plt.plot(ns, phpq_lists, label='Python Heap Priority Queue')
+plt.title('Elapsed Time Vs. \'ns\' for Three Priority Queues')
+plt.xlabel('ns (# of Lists Merged)')
+plt.ylabel('Elapsed Time (Seconds)')
 plt.legend()
 plt.show()
