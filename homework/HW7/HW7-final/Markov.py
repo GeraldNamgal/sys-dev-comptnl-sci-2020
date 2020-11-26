88d82117f575673c1d372739704bfa045398bc98
mappia <gerald.arocena@gmail.com>
2020-11-26 07:56:24 -0500

    Saving progress / checking changes.

--- homework/HW7/HW7-final/Markov.py
+++ homework/HW7/HW7-final/Markov.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import random
 
 
 class Markov:
+    mappings = {'sunny': 0, 'cloudy': 1, 'rainy': 2, 'snowy': 3, 'windy': 4,
+                'hailing': 5}
+
     def __init__(self):  # You will need to modify this header line later in Part C
         self.data = []
 
@@ -20,8 +24,26 @@ class Markov:
                 raise ValueError('Invalid weather type entered')
         except Exception:
             raise
-        return self.data[self.mappings[current_day_weather],
-                         self.mappings[next_day_weather]]
+        row = self.mappings[current_day_weather]
+        col = self.mappings[next_day_weather]
+        return self.data[row][col]
 
-    mappings = {'sunny': 0, 'cloudy': 1, 'rainy': 2, 'snowy': 3, 'windy': 4,
-                'hailing': 5}
+    def __iter__(self):
+        return MarkovIterator(self)
+
+
+# Referenced https://pynative.com/python-weighted-random-choices-with-probability/
+class MarkovIterator:
+    def __init__(self, markov):
+        self.markov = markov
+        self.number_list = []
+        for key, val in self.markov.mappings:
+            self.number_list.append(val)
+
+    def __next__(self):
+        row = 1  # TODO: Row should be current day
+        col = random.choices(self.number_list, tuple(self.markov.data[row]))
+        return self.markov.data[row][col]
+
+    def __iter__(self):
+        self
