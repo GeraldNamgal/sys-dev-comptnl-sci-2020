f43b8a9ad0ee9e493d5ba11a8b84fbf959c76b6d
mappia <gerald.arocena@gmail.com>
2020-11-26 08:41:19 -0500

    Saving progress.

--- homework/HW7/HW7-final/Markov.py
+++ homework/HW7/HW7-final/Markov.py
@@ -8,8 +8,11 @@ class Markov:
     mappings = {'sunny': 0, 'cloudy': 1, 'rainy': 2, 'snowy': 3, 'windy': 4,
                 'hailing': 5}
 
-    def __init__(self):  # You will need to modify this header line later in Part C
+    def __init__(self, day_zero_weather=None):
         self.data = []
+        self.day_zero_weather = day_zero_weather
+        self._current_day = 0
+        self._current_day_weather = day_zero_weather
 
     # Referenced https://janakiev.com/blog/csv-in-python/
     def load_data(self, file_path='./weather.csv'):
@@ -28,8 +31,35 @@ class Markov:
         col = self.mappings[next_day_weather]
         return self.data[row][col]
 
+    # TODO: Supposed to be instance method (no self in HW instructions)?
+    def get_weather_for_day(self, day, trials=3):  # TODO: trials default ok?
+        trials_list = []
+        for trial in range(trials):
+            for day in range(1, day):
+                self._simulate_weather_for_day(day)
+
+    # TODO: Supposed to be instance method (no self in HW instructions)?
+    def _simulate_weather_for_day(self, day):
+        pass
+
     def __iter__(self):
         return MarkovIterator(self)
+
+    @property
+    def current_day(self):
+        return self._current_day
+
+    @current_day.setter
+    def current_day(self, into):
+        self._current_day = into
+
+    @property
+    def current_day_weather(self):
+        return self._current_day_weather
+
+    @current_day_weather.setter
+    def current_day_weather(self, into):
+        self._current_day_weather = into
 
 
 # Referenced https://pynative.com/python-weighted-random-choices-with-probability/
@@ -37,7 +67,7 @@ class MarkovIterator:
     def __init__(self, markov):
         self.markov = markov
         self.number_list = []
-        for key, val in self.markov.mappings:
+        for key, val in markov.mappings:
             self.number_list.append(val)
 
     def __next__(self):
