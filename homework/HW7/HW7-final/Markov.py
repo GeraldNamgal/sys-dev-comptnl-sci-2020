15ffdecf88d7d38b8d59161050d5966f917c55fd
mappia <gerald.arocena@gmail.com>
2020-11-27 09:24:28 -0500

    Added TODO.

--- homework/HW7/HW7-final/Markov.py
+++ homework/HW7/HW7-final/Markov.py
@@ -84,6 +84,7 @@ class MarkovIterator:
     def __init__(self, markov):
         self.markov = markov
 
+    # TODO: Need to raise an exception per https://piazza.com/class/kc57xuuysdm64b?cid=542
     def __next__(self):
         if self.markov.current_day == 0 and self.markov.days_out == 0:
             next_day_weather = self.markov.current_day_weather
