fddaedbd40cc0d31e3f22af30b61d3310305f511
mappia <gerald.arocena@gmail.com>
2020-11-28 03:51:53 -0500

    Think done HW7.

--- homework/HW7/HW7-final/P2.py
+++ homework/HW7/HW7-final/P2.py
@@ -58,8 +58,8 @@ def save_to_database(model_id, model_desc, db, model,
 
     # Goes in model_results
     cursor.execute('''INSERT INTO model_results
-        (id, desc, train_score, test_score)
-        VALUES (?, ?, ?, ?)''',
+    (id, desc, train_score, test_score)
+    VALUES (?, ?, ?, ?)''',
                    (model_id, model_desc, model.score(X_train, y_train),
                     model.score(X_test, y_test)))
     db.commit()
@@ -107,19 +107,52 @@ penalized_model.fit(X_train, y_train)
 save_to_database(3, 'L1 penalty model', db, penalized_model,
                  X_train, X_test, y_train, y_test)
 
+# Part C
+high_scorer = '''SELECT id, test_score FROM model_results WHERE test_score =
+            (SELECT MAX(test_score) FROM model_results)'''
+cursor.execute(high_scorer)
+tuple = cursor.fetchall()[0]
+id_best = tuple[0]
+score_best = tuple[1]
+print(f'Best model id: {id_best}\nBest validation score: {score_best}')
+print('\nBest model\'s features\n---------------------')
+features_best = f'''SELECT feature_name, value FROM model_coefs
+                    WHERE id = {id_best}'''
+cursor.execute(features_best)
+features_list = cursor.fetchall()
+for feature, val in features_list:
+    print(f'{feature}: {val}')
 
-# TODO: Debugging
-cursor.execute("SELECT * FROM model_params")
-for row in cursor.fetchall():
-    print(row)
-cursor.execute("SELECT * FROM model_coefs")
-for row in cursor.fetchall():
-    print(row)
-cursor.execute("SELECT * FROM model_results")
-for row in cursor.fetchall():
-    print(row)
+test_model = LogisticRegression(solver='liblinear')
+test_model.fit(X_train, y_train)
+
+# Manually change fit parameters
+intercept = features_list[0][1]
+coef = []
+for feature in features_list[1:]:
+    coef.append(feature[1])
+test_model.coef_ = np.array([coef])
+test_model.intercept_ = np.array([intercept])
+print()
+
+test_score = test_model.score(X_test, y_test)
+print(f'Reproduced best validation score: {test_score}')
+
+# # TODO: Debugging -- View the tables
+# cursor.execute("SELECT * FROM model_params")
+# for row in cursor.fetchall():
+#     print(row)
+# cursor.execute("SELECT * FROM model_coefs")
+# for row in cursor.fetchall():
+#     print(row)
+# cursor.execute("SELECT * FROM model_results")
+# for row in cursor.fetchall():
+#     print(row)
+
+db.commit()
+db.close()
 
 
-# Referenced:
+# References
 # https://datascience.stackexchange.com/questions/29131/feature-names-in-logisticregression
 # https://stackoverflow.com/questions/57924484/finding-coefficients-for-logistic-regression-in-python
